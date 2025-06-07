17fd9908bf7928ada3175066bd038805a46b034f
Noah Bright <noah.bright.1@gmail.com>
2025-06-07 10:25:15 -0700

    fix netrw c-l keymap, remove nvim dap

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -20,6 +20,13 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "netrw",
+    callback = function()
+        -- Override <C-l> in netrw to do window movement
+        vim.keymap.set("n", "<C-l>", "<C-w>l", { buffer = true, remap = true })
+    end,
+})
 
 vim.keymap.set('n', '<leader>sv', '<C-w>s', { desc = 'split vertically' })
 vim.keymap.set('n', '<leader>sh', '<C-w>v', { desc = 'split horizontally' })
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,7 +2,6 @@ return {
     require('plugins.colorscheme'),
     require('plugins.lspconfig'),
     require('plugins.nvim-cmp'),
-    require('plugins.nvim-dap'),
     require('plugins.telescope'),
     require('plugins.treesitter'),
     require('plugins.webdevicons'),
