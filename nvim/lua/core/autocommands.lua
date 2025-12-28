2c2f387bbfe97e81d391083e309831f547aabf8d
Noah Bright <noah.bright.1@gmail.com>
2025-12-28 09:15:34 -0500

    open help in a vertical split

--- nvim/lua/core/autocommands.lua
+++ nvim/lua/core/autocommands.lua
@@ -4,16 +4,26 @@
 --  See `:help vim.highlight.on_yank()`
 --  dont understand groups yet
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 vim.api.nvim_create_autocmd("BufWrite", {
     desc = "format on write",
     callback = function()
         vim.lsp.buf.format()
+    end
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    desc = 'Open help in a vertical split',
+    callback = function()
+        local buftype = vim.api.nvim_buf_get_option(0, 'buftype')
+        if buftype == "help" then
+            vim.api.nvim_feedkeys("L", "n", false)
+        end
     end
 })
